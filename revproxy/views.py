634680a5e819da8246fb98a2cc46f37a3071a113
Ryan Malloy <ryan@supported.systems>
2024-04-22 04:33:56 -0600

    get_email for REMOTE_USER

--- revproxy/views.py
+++ revproxy/views.py
@@ -149,7 +149,7 @@ class ProxyView(View):
 
         if (self.add_remote_user and hasattr(self.request, 'user')
                 and self.request.user.is_active):
-            request_headers['REMOTE_USER'] = self.request.user.get_username()
+            request_headers['REMOTE_USER'] = self.request.user.get_email()
             self.log.info("REMOTE_USER set")
 
         if self.add_x_forwarded:
